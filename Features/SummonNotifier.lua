d5a681e439754a95f7004c44f176ad2d4cadb0cb
Valentin Henique <vhenique@gmail.com>
2026-07-19 18:12:50 +0200

    fix: summon table message

--- Features/SummonNotifier.lua
+++ Features/SummonNotifier.lua
@@ -1,7 +1,7 @@
 local _, GraalHelper = ...
 
 function Summoning(spellIDs, spellID, message)
-    if (not spellIDs[spellID]) then return false end
+    if (not spellIDs[spellID]) then return end
     if IsInRaid() then
         SendChatMessage(message, "RAID")
     else
@@ -10,34 +10,39 @@ function Summoning(spellIDs, spellID, message)
     return true
 end
 
-function SummoningPeople(spellID)
+function SummoningPeopleByStone(spellID)
     local target = GetUnitName("target", false)
     if target == nil then return end
-    local trackedSpells = {
-        [23598] = true, -- 23598 = summoning stone (channeled)
-        [698] = true,   -- 698 = ritual of summoning (initial cast, not the channel)
-    }
+    -- 23598 = summoning stone (channeled)
+    local trackedSpells = { [23598] = true }
+    return Summoning(trackedSpells, spellID, "-> Summoning " .. target .. ". Please click! <-")
+end
+
+function SummoningPeopleByRitual(spellID)
+    local target = GetUnitName("target", false)
+    if target == nil then return end
+    -- 698 = ritual of summoning (initial cast, not the channel)
+    local trackedSpells = { [698] = true }
     return Summoning(trackedSpells, spellID, "-> Summoning " .. target .. ". Please click! <-")
 end
 
 function SummoningRitualOfSouls(spellID)
-    local trackedSpells = {
-        [29893] = true, -- 29893 = ritual of souls
-    }
+    -- 29893 = ritual of souls
+    local trackedSpells = { [29893] = true }
     return Summoning(trackedSpells, spellID, "-> Summoning Candy. Please click! <-")
 end
 
 function SummoningRitualOfRefreshment(spellID)
-    local trackedSpells = {
-        [43987] = true, -- 43987 = Ritual of refreshment
-    }
+    -- 43987 = Ritual of refreshment
+    local trackedSpells = { [43987] = true }
     return Summoning(trackedSpells, spellID, "-> Summoning Refreshment. Please click! <-")
 end
 
 function GraalHelper:SummonNotify(caster, spellID)
     local desactivate = not self.config.summonNotifier.active
     if desactivate or caster ~= "player" or IsInGroup() == false then return end
-    if SummoningPeople(spellID) then return end
-    if SummoningRitualOfSouls(spellID) then return end
-    if SummoningRitualOfRefreshment() then return end
+    SummoningPeopleByStone(spellID)
+    SummoningPeopleByRitual(spellID)
+    SummoningRitualOfSouls(spellID)
+    SummoningRitualOfRefreshment(spellID)
 end
